d0be7482cf82e8488a863bc467f05c0cf36538f8
Mario Balibrera <mario@gameclosure.com>
2013-05-02 11:19:03 -0700

    respect loop false for background sounds

--- src/native/Audio.js
+++ src/native/Audio.js
@@ -100,7 +100,7 @@ var Audio = exports = Class(function () {
 		if (this.isBackgroundMusic) {
 			this._startedLoad = true;
 			this._startTime = Date.now();
-			NATIVE.sound.playBackgroundMusic(this._src, this._volume, true);
+			NATIVE.sound.playBackgroundMusic(this._src, this._volume, this.loop);
 		} else if (!this._startedLoad) {
 			this.load(true);
 		} else {
